74a497fffd67d9a865144976b7bae203ad96d5f1
LucijaZakarija <73385829+LucijaZakarija@users.noreply.github.com>
2023-12-27 12:31:55 +0100

    Add files via upload

---
+++ convertVideo2Frames.py
@@ -0,0 +1,12 @@
+import cv2
+vidcap = cv2.VideoCapture('velikiParking.mp4') #vidi treba li path
+success,image = vidcap.read()
+count = 0
+fps = vidcap.get(cv2.CAP_PROP_FPS)
+print('frames per second =',fps)
+while success: #smanji broj fps za vecu razliku 
+  if count%29==0:
+    cv2.imwrite("C:/FER/Diplomski/2.GODINA/Diplomski rad/Training set/Images/kratkiSegmenti/velikiParking%d.jpg" % count, image)     # save frame as JPEG file      
+  success,image = vidcap.read()
+  print('Read a new frame: ', success)
+  count += 1
